1dc5273ee84d230d9535a75a1583056d1b285d3e
MrGeo <cocoplayz555@gmail.com>
2025-07-29 14:09:23 +0200

    fixed update checker hopefully :D

--- update_checker.py
+++ update_checker.py
@@ -51,7 +51,7 @@ def update_check():
             
             if latest_version != current_version:
                 root.mainloop()
-                return f"Update available: {latest_version}"
+                return latest_version
         except Exception as e:
             return f"Error checking version: {e}"
 
